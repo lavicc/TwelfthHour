ffcb4b81f3b108b539f28dd9af7b64743fd6100c
Brandon Blakemore <brandon.blakemore@brutifulgames.com>
2020-09-07 15:07:14 -0500

    Character Movement tests
    half implemented character movement
    2d Animation not working properly

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, y, 0);
     }
 }
